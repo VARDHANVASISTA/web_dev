e719228dd2897ac81b54b5481702ddee2b75253f
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2024-03-11 17:24:07 +0530

    Update dicee.js

--- dicee/dicee.js
+++ dicee/dicee.js
@@ -1,15 +1,17 @@
-var rN1 = Math.floor(Math.random()*6)+1;
-var c1 = "/images/dice"+rN1+".png";  
-document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c1);
-var rN2 = Math.floor(Math.random()*6)+1;
-var c2 = "/images/dice"+rN1+".png";  
-document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c2);
-if (rN1 === rN2){
-    document.querySelector("h1").textContent="It's a tie!";
-}
-else if (rN1 > rN2) {
-    document.querySelector("h1").textContent="Player 1 wins!";
-} 
-else if (rN1 < rN2) {
-    document.querySelector("h1").textContent="Player 2 wins!";
+function execute(){
+        var rN1 = Math.floor(Math.random()*6)+1;
+    var c1 = "/images/dice"+rN1+".png";  
+    document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c1);
+    var rN2 = Math.floor(Math.random()*6)+1;
+    var c2 = "/images/dice"+rN1+".png";  
+    document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c2);
+    if (rN1 === rN2){
+        document.querySelector("h4").textContent="It's a tie!";
+    }
+    else if (rN1 > rN2) {
+        document.querySelector("h4").textContent="Player 1 wins!";
+    } 
+    else if (rN1 < rN2) {
+        document.querySelector("h4").textContent="Player 2 wins!";
+    }
 }
