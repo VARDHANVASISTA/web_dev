8d68be3f05ebcce8da64a195c4a6372feda8e251
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2024-03-19 15:15:46 +0530

    Update dicee.js

--- dicee/dicee.js
+++ dicee/dicee.js
@@ -1,10 +1,10 @@
 function execute(){
-        var rN1 = Math.floor(Math.random()*6)+1;
+    var rN1 = Math.floor(Math.random()*6)+1;
     var c1 = "/images/dice"+rN1+".png";  
     document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c1);
     var rN2 = Math.floor(Math.random()*6)+1;
-    var c2 = "/images/dice"+rN1+".png";  
-    document.getElementsByClassName("dice")[0].querySelector("img").setAttribute("src", c2);
+    var c2 = "/images/dice"+rN2+".png";  
+    document.getElementsByClassName("dice")[1].querySelector("img").setAttribute("src", c2);
     if (rN1 === rN2){
         document.querySelector("h4").textContent="It's a tie!";
     }
